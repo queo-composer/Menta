b900fcbdc68311da8450c1b60fefe2352a93d1d9
Fabrizio Branca <fabrizio.branca@aoemedia.de>
2013-05-14 12:57:40 -0700

    Added createRandomString()

--- lib/Menta/Util/Div.php
+++ lib/Menta/Util/Div.php
@@ -16,4 +16,15 @@ class Menta_Util_Div {
 		return "contains(concat(' ', @$attribute, ' '), ' $needle ')";
 	}
 
+	/**
+	 * Create random string
+	 *
+	 * @param int $length
+	 * @param string $chars
+	 * @return string
+	 */
+	public static function createRandomString($length = 8, $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") {
+		return substr(str_shuffle($chars), 0, $length);
+	}
+
 }
